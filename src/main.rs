4210545db41f1f023a885b01646a1edee85d1648
Radical <radical@radical.fun>
2025-05-01 03:53:44 +0200

    feat: add tables for refresh/access tokens

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ async fn main() -> Result<(), Error> {
         CREATE TABLE IF NOT EXISTS users (
             uuid uuid PRIMARY KEY UNIQUE NOT NULL,
             username varchar(32) UNIQUE NOT NULL,
-            display_name varchar(64),
+            display_name varchar(64) DEFAULT NULL,
             password varchar(512) NOT NULL,
             email varchar(100) UNIQUE NOT NULL,
             email_verified boolean NOT NULL DEFAULT FALSE
@@ -52,6 +52,17 @@ async fn main() -> Result<(), Error> {
         CREATE TABLE IF NOT EXISTS instance_permissions (
             uuid uuid REFERENCES users(uuid),
             administrator boolean NOT NULL DEFAULT FALSE
+        );
+        CREATE TABLE IF NOT EXISTS refresh_tokens (
+            token varchar(64) PRIMARY KEY UNIQUE NOT NULL,
+            uuid uuid REFERENCES users(uuid),
+            created int8 NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS access_tokens (
+            token varchar(32) PRIMARY KEY UNIQUE NOT NULL,
+            refresh_token varchar(64) UNIQUE REFERENCES refresh_tokens(token),
+            uuid uuid REFERENCES users(uuid),
+            created int8 NOT NULL
         )
     "#)
     .execute(&pool)
