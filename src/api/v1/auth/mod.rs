83872ed7a68e098d21b503b1f5fcf45eb6b52467
Radical <radical@radical.fun>
2025-05-01 07:06:14 +0200

    feat: add a function to check access token
    lets me reuse something that will happen often instead of having to write it manually in every file

--- src/api/v1/auth/mod.rs
+++ src/api/v1/auth/mod.rs
@@ -1,4 +1,8 @@
-use actix_web::{Scope, web};
+use std::{str::FromStr, time::{SystemTime, UNIX_EPOCH}};
+
+use actix_web::{web, HttpResponse, Scope};
+use sqlx::Postgres;
+use uuid::Uuid;
 
 mod register;
 mod login;
@@ -9,4 +13,29 @@ pub fn web() -> Scope {
         .service(register::res)
         .service(login::response)
         .service(refresh::res)
+}
+
+pub async fn check_access_token(access_token: String, pool: sqlx::Pool<Postgres>) -> Result<Uuid, HttpResponse> {
+    match sqlx::query_as("SELECT CAST(uuid as VARCHAR), created FROM access_tokens WHERE token = $1")
+        .bind(&access_token)
+        .fetch_one(&pool)
+        .await {
+        Ok(row) => {
+            let (uuid, created): (String, i64) = row;
+
+            let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64;
+        
+            let lifetime = current_time - created;
+        
+            if lifetime > 3600 {
+                return Err(HttpResponse::Unauthorized().finish())
+            }
+        
+            Ok(Uuid::from_str(&uuid).unwrap())
+        },
+        Err(error) => {
+            eprintln!("{}", error);
+            Err(HttpResponse::InternalServerError().finish())
+        }
+    }
 }
