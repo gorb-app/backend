af7193750686de6d3ebfcb7cbe3f091c19008671
BAaboe <benjamin.mjaatvedt@gmail.com>
2025-07-22 22:01:48 +0200

    fix: fixed the ban table names

--- migrations/2025-07-22-195121_add_ban/up.sql
+++ migrations/2025-07-22-195121_add_ban/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE guild_bans (
-	guild_ban uuid NOT NULL REFERENCES guilds(uuid) ON DELETE CASCADE,
-	user_ban uuid NOT NULL REFERENCES users(uuid),
+	guild_uuid uuid NOT NULL REFERENCES guilds(uuid) ON DELETE CASCADE,
+	user_uuid uuid NOT NULL REFERENCES users(uuid),
 	reason VARCHAR(200) DEFAULT NULL,
 	PRIMARY KEY (user_uuid, guild_uuid)
 );
