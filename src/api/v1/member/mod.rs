0468d1adca6d2011fb538219263b8b22b2deaeaa
BAaboe <benjamin.mjaatvedt@gmail.com>
2025-07-22 18:05:19 +0200

    fix: Unecessary merge of routers

--- src/api/v1/member/mod.rs
+++ src/api/v1/member/mod.rs
@@ -11,11 +11,8 @@ use crate::{AppState, api::v1::auth::CurrentUser};
 mod uuid;
 
 pub fn router(app_state: Arc<AppState>) -> Router<Arc<AppState>> {
-    let router_with_auth = Router::new()
+    Router::new()
         .route("/{uuid}", get(uuid::get))
         .route("/{uuid}", delete(uuid::delete))
-        .layer(from_fn_with_state(app_state, CurrentUser::check_auth_layer));
-
-    Router::new()
-        .merge(router_with_auth)
+        .layer(from_fn_with_state(app_state, CurrentUser::check_auth_layer))
 }
